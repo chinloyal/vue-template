4764114d8e8f851656e2bbc427637403031c6e67
chinloyal <jordain7@gmail.com>
2020-03-16 12:45:29 -0500

    Disabled up to date branch rule

--- .hooks/pre-push.js
+++ .hooks/pre-push.js
@@ -3,6 +3,6 @@ const branch = require("./general/branch");
 const exec = require("child_process").execSync;
 
 branch.checkName(exec);
-branch.upToDate(exec);
+// branch.upToDate(exec);
 
 log.success("Yuh code up, go through.");
